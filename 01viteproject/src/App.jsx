f956dace5cfb27a1cd328e18ba4e9ec980dc94dd
Raviraj Patil <ravirajkpatil06@gmail.com>
2024-09-24 16:59:18 +0530

    project creations with basic react and Vite

---
+++ 01viteproject/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./Chai"
+
+function App() {
+  return (
+     <>        //  call this Fragment also we can use div
+      <Chai/>
+      <h2>Vite project by Riddhesh</h2>
+    </>
+  )
+}
+
+export default App
