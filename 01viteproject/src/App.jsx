5e49e6dd1c3fe0da87279300e13e62c6aa229542
Raviraj Patil <ravirajkpatil06@gmail.com>
2024-09-25 18:11:26 +0530

    create own jsx library in react and some custom react methods

--- 01viteproject/src/App.jsx
+++ 01viteproject/src/App.jsx
@@ -1,10 +1,11 @@
 import Chai from "./Chai"
 
 function App() {
+  const username="with React"
   return (
      <>        //  call this Fragment also we can use div
       <Chai/>
-      <h2>Vite project by Riddhesh</h2>
+       <h2>Vite project by Riddhesh {username}</h2> {/* username is a evaluated expression */}
     </>
   )
 }
--- 01viteproject/src/main.jsx
+++ 01viteproject/src/main.jsx
@@ -2,9 +2,39 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-
+//---------------------// function calling is working
+function MyApp() {
+  return(
+    <div>
+      <h1>Custom App!!</h1>
+    </div>
+  )
+}
+//------------------
+const reactElement = {
+  type: 'a',
+  props: {
+      href: 'https:google.com',
+      target: '_blank'
+  },
+  children: 'Click here to visit google'
+}
+//--------------------
+const anotherElement = (
+  <a href='https://google.com' target='_blank'>Visit google</a>
+)
+//--------------------
+const username = "raviraj"
+const secondElement = (
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'Click here to visit',
+  username
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+  <App />
+   {/* <MyApp/> */}
+  {/* secondElement  It is no tworking */}
+  </>
 )
