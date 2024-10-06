77a8139294d561bbaa7040a86752d05d68b1fbb3
Raviraj Patil <ravirajkpatil06@gmail.com>
2024-10-06 16:43:36 +0530

    currency converter project, the api is now not available

---
+++ 06currencyconverter/src/components/index.js
@@ -0,0 +1,6 @@
+// This js file created for gathering the all jsx files here
+// and after gathering we reffer this js file in main render.
+
+import InputBox from "./InputBox";
+
+export {InputBox}
